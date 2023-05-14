09d5abf88c5278117489264c49bb950ce18893ee
oibioib <pavelbodal@gmail.com>
2023-05-14 17:08:57 +0200

    fix: clear api constants

--- src/constants/apiSettings.ts
+++ src/constants/apiSettings.ts
@@ -3,7 +3,5 @@ export const API_CREDENTIALS = {};
 export const API_BASE_URL = 'https://temp.com';
 
 export const API_ENDPOINTS = {
-  TENP: '/temp',
-  VACANCIES: '/vacancies',
-  CATALOGUES: '/catalogues',
+  TEMP: '/temp',
 };
