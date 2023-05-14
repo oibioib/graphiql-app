74aa1bb0434333785d59fda93de34b1ad0af2144
oibioib <pavelbodal@gmail.com>
2023-05-14 22:07:06 +0200

    feat: add step validation for adding password

--- src/pages/AuthentificationPage/AuthentificationPage.tsx
+++ src/pages/AuthentificationPage/AuthentificationPage.tsx
@@ -9,40 +9,69 @@ import {
   Text,
   TextInput,
 } from '@mantine/core';
-import { isEmail, useForm } from '@mantine/form';
+import { useForm, zodResolver } from '@mantine/form';
+import { z } from 'zod';
+
+const schema = z.object({
+  email: z.string().email({ message: 'Invalid email' }),
+  password: z.string().superRefine((val, ctx) => {
+    const issues = [];
+
+    if (val.length < 8) {
+      issues.push('8 chars');
+    }
+
+    if (!/[A-Za-z]/.test(val)) {
+      issues.push('one letter');
+    }
+
+    if (!/[0-9]/.test(val)) {
+      issues.push('one digit');
+    }
+
+    if (!/[#?!@$%^&*-]/.test(val)) {
+      issues.push('one special character');
+    }
+
+    if (issues.length) {
+      const errorMessage = `At least ${issues.join(', ')}.`;
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: errorMessage,
+      });
+    }
+  }),
+});
 
 const AuthentificationPage = () => {
   const form = useForm({
+    validateInputOnChange: true,
+    validate: zodResolver(schema),
     initialValues: {
       email: '',
       password: '',
-    },
-
-    validate: {
-      email: isEmail('Invalid email'),
-      password: (value) =>
-        /^(?=.*?[A-Za-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/.test(value)
-          ? null
-          : 'Invalid password',
     },
   });
 
   return (
     <Flex align="center" h="100%">
-      <Container size={420} p={0}>
+      <Container size={440} p={0} w="100%">
         <Paper withBorder shadow="md" p={30} radius="md">
-          <form onSubmit={form.onSubmit((values) => console.log(values))}>
+          <form
+            onSubmit={form.onSubmit((values) => {
+              form.reset();
+              console.log(values);
+            })}
+          >
             <TextInput
               {...form.getInputProps('email')}
               label="Email"
-              placeholder="you@mantine.dev"
-              required
+              placeholder="email@liame.com"
             />
             <PasswordInput
               {...form.getInputProps('password')}
               label="Password"
               placeholder="Your password"
-              required
               mt="md"
             />
             <Button fullWidth mt="xl" type="submit">
