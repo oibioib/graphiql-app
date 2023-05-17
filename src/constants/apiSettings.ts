e203c111b3c2a8fc5b1ce3deab6a9a4359ed96c3
Aleksandr <a.teslin90@gmail.com>
2023-05-17 16:01:37 +0300

    add: add constants

--- src/constants/apiSettings.ts
+++ src/constants/apiSettings.ts
@@ -1,6 +1,6 @@
 export const API_CREDENTIALS = {};
 
-export const API_BASE_URL = 'https://temp.com';
+export const API_BASE_URL = 'https://countries.trevorblades.com/graphql';
 
 export const API_ENDPOINTS = {
   TEMP: '/temp',
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,6 @@
 import * as API_SETTINGS from './apiSettings';
 import * as APP_SETTINGS from './appSettings';
+import * as SCHEMA_QUERY from './schemaQuery';
 import * as STORAGE_SETTINGS from './storageSettings';
 
-export { API_SETTINGS, APP_SETTINGS, STORAGE_SETTINGS };
+export { API_SETTINGS, APP_SETTINGS, STORAGE_SETTINGS, SCHEMA_QUERY };
