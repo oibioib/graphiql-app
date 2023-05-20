5d2038eb2bc89ee4fa15423342550ba903cbfc00
alchonokk <galunia_suglob@mail.ru>
2023-05-20 11:51:06 +0300

    feat: add protectedRoute

---
+++ src/components/CheckAuth/CheckAuth.tsx
@@ -0,0 +1,23 @@
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Navigate } from 'react-router-dom';
+
+import { Loader } from '@components';
+import { APP_SETTINGS } from '@constants';
+import { auth } from '@helpers/firebase';
+
+interface ProtectedRouteProps {
+  outlet: JSX.Element;
+}
+
+const CheckAuth = ({ outlet }: ProtectedRouteProps) => {
+  const [user, loading] = useAuthState(auth);
+  if (loading) {
+    return <Loader />;
+  }
+  if (user) {
+    return <Navigate to={APP_SETTINGS.PAGES.GRAPHQL.ROUTE} replace />;
+  }
+  return outlet;
+};
+
+export default CheckAuth;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,6 @@
+import CheckAuth from './CheckAuth/CheckAuth';
 import Loader from './Loader/Loader';
+import NavigateMenu from './NavigateMenu/NavigateMenu';
+import ProtectedRoute from './ProtectedRoute/ProtectedRoute';
 
-export { Loader };
+export { Loader, NavigateMenu, ProtectedRoute, CheckAuth };
