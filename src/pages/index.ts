99465dcc8bef3422b416d378923a565989b0d9b2
alchonokk <galunia_suglob@mail.ru>
2023-05-20 11:50:08 +0300

    refactor: update export

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,7 @@
 import AuthentificationPage from './AuthentificationPage/AuthentificationPage';
 import ErrorPage from './ErrorPage/ErrorPage';
 import GraphQlPage from './GraphQlPage/GraphQlPage';
+import LogInPage from './LogInPage/LogInPage';
 import WelcomePage from './WelcomePage/WelcomePage';
 
-export { GraphQlPage, WelcomePage, ErrorPage, AuthentificationPage };
+export { GraphQlPage, WelcomePage, ErrorPage, AuthentificationPage, LogInPage };
